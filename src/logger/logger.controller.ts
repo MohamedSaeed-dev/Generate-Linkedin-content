09b4efcff9a5233c039fd75f37566657f52e3829
Mohamed-Sa2024 <mohammedsaeed@cleanlife.sa>
2025-04-29 08:37:42 +0300

    private logs

--- src/logger/logger.controller.ts
+++ src/logger/logger.controller.ts
@@ -1,10 +1,8 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { PaginationDto } from 'src/dto/pagination.dto';
 import { LoggerService } from './logger.service';
-import { Public } from 'src/decorators/public.decorator';
 
 @Controller('logs')
-@Public()
 export class LoggerController {
     constructor(private readonly loggerService: LoggerService) { }
 

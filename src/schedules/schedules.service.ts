4e6ee5fa25e9e59ec57c39f0081f28aca3def807
Mohamed-Sa2024 <mohammedsaeed@cleanlife.sa>
2025-05-04 16:32:25 +0300

    fix cron job to 5PM

--- src/schedules/schedules.service.ts
+++ src/schedules/schedules.service.ts
@@ -6,7 +6,7 @@ export class SchedulesService {
   constructor(private readonly llmService: LlmService) { }
   private readonly logger = new Logger(SchedulesService.name);
 
-  @Cron('0 15 * * *', {
+  @Cron('0 0 17 * * *', {
     timeZone: 'Asia/Riyadh'
   })
   async handleCron() {

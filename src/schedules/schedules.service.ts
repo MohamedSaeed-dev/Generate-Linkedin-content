5ad89a8277ac5217bbe6148d4726912e4f275ce7
Mohamed-Sa2024 <mohammedsaeed@cleanlife.sa>
2025-05-04 08:18:36 +0300

    cronjob timezone

--- src/schedules/schedules.service.ts
+++ src/schedules/schedules.service.ts
@@ -6,11 +6,11 @@ export class SchedulesService {
   constructor(private readonly llmService: LlmService) { }
   private readonly logger = new Logger(SchedulesService.name);
 
-  @Cron('0 15 * * *')
+  @Cron('0 15 * * *', {
+    timeZone: 'Asia/Riyadh'
+  })
   async handleCron() {
     this.logger.debug('Cron job running at 3:00 PM daily');
     await this.llmService.askAutomated();
   }
-
-
 }
